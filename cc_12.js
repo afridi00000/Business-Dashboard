b3a4cd4ae63101742120f48906387560da096451
afridi00000 <afridi@usf.edu>
2025-02-26 16:14:10 -0500

    Task 1 - Created Revenue Metric Card

---
+++ cc_12.js
@@ -0,0 +1,9 @@
+// Task 1: Business Dashboard – DOM Element Selection and Creation
+const dashboard = document.getElementById("dashboard");
+
+const revenueCard = document.createElement("div");
+revenueCard.setAttribute("class", "metric-card");
+revenueCard.setAttribute("id", "revenueCard");
+revenueCard.innerHTML = "<h3>Revenue</h3><p>$0</p>";
+
+dashboard.appendChild(revenueCard);
